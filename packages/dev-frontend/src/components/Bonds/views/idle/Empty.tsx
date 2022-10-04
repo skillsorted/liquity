c721b1ddb2eccdb9aeb06e40bb623539f88ebbe0
skillsorted <contact@skillsorted.com>
2022-10-04 23:22:54 +0100

    Bond docs link

--- packages/dev-frontend/src/components/Bonds/views/idle/Empty.tsx
+++ packages/dev-frontend/src/components/Bonds/views/idle/Empty.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "theme-ui";
 import { InfoMessage } from "../../../InfoMessage";
 import { NOT_BONDED_YET } from "../../lexicon";
 
 export const Empty: React.FC = () => {
-  return <InfoMessage title={NOT_BONDED_YET.term}>{NOT_BONDED_YET.description}</InfoMessage>;
+  return <InfoMessage title={NOT_BONDED_YET.term}>{NOT_BONDED_YET.description}
+    <br></br>
+    <br></br>
+    Learn more: <Link href={`https://liquity.gitbook.io/chicken-bonds/`} target="_blank">What is a bond?</Link>
+  </InfoMessage>;
+
 };
