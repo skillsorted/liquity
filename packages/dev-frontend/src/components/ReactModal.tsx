48fa9737f566285d03d9895258662b2b25e241bd
skillsorted <contact@skillsorted.com>
2022-10-05 11:40:11 +0100

    Dark reactModal

--- packages/dev-frontend/src/components/ReactModal.tsx
+++ packages/dev-frontend/src/components/ReactModal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
-
+import { useColorMode } from "theme-ui";
 const modalStyle = {
   content: {
     top: "50%",
@@ -11,7 +11,23 @@ const modalStyle = {
     transform: "translate(-50%, -50%)",
     maxWidth: "620px",
     width: "580px",
-    maxHeight: "94vh"
+    maxHeight: "94vh",
+  },
+  overlay: { zIndex: 2 }
+};
+
+const modalStyleDark = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    maxWidth: "620px",
+    width: "580px",
+    maxHeight: "94vh",
+    background: "#2c2c2c"
   },
   overlay: { zIndex: 2 }
 };
@@ -23,14 +39,21 @@ type ModalProps = {
 
 export const ReactModal: React.FC<ModalProps> = ({ children, onDismiss, style }) => {
   const handleDismiss = () => onDismiss();
+  const [colorMode] = useColorMode();
 
-  return (
-    <Modal
+  return colorMode === 'dark' ?
+    (<Modal
+      isOpen={true}
+      onRequestClose={handleDismiss}
+      style={{ ...modalStyleDark, content: { ...modalStyleDark.content, ...style } }}
+    >
+      {children}
+    </Modal>) : (<Modal
       isOpen={true}
       onRequestClose={handleDismiss}
       style={{ ...modalStyle, content: { ...modalStyle.content, ...style } }}
     >
       {children}
-    </Modal>
-  );
+    </Modal>);
+
 };
